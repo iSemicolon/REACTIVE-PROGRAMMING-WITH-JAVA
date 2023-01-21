79d75ca0cf6ba7d741e6d80acb528449a4d2e296
iSemicolon <palash.samanta7497@gmail.com>
2023-01-21 17:12:08 +0530

    With Using Stream

--- Reactive-Stream-Demo/src/Demo1.java
+++ Reactive-Stream-Demo/src/Demo1.java
@@ -1,35 +1,48 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Demo1 {
 
 	public static void main(String[] args) {
-      ArrayList<Integer> numberss = new ArrayList<>();
-      numberss.add(10);
-      numberss.add(15);
-      numberss.add(30);
-      numberss.add(45);
-      numberss.add(50);
 
-      System.out.println(numberss);
-      
-      List<Integer> numbers = Arrays.asList(10, 15, 20, 25, 30);
-      System.out.println(numbers);
-      
-      List<Integer> evenNumbers = new ArrayList<>();
+		System.out.println("without using stream...............................");
 
-      
+		ArrayList<Integer> numberss = new ArrayList<>();
+		numberss.add(10);
+		numberss.add(15);
+		numberss.add(30);
+		numberss.add(45);
+		numberss.add(50);
 
-      // without using stream
-      for(int number: numbers){
-          if(number % 2 == 0){
-              evenNumbers.add(number);
-          }
-      }
+		System.out.println(numberss);
 
-      System.out.println(evenNumbers);
+		List<Integer> numbers = Arrays.asList(10, 15, 20, 25, 30);
+		System.out.println(numbers);
+
+		List<Integer> evenNumbers = new ArrayList<>();
+
+
+
+		// without using stream
+		for(int number: numbers){
+			if(number % 2 == 0){
+				evenNumbers.add(number);
+			}
+		}
+
+		System.out.println(evenNumbers);
+
+
+		System.out.println("with using stream...............................");
+
+		// with using stream
+		evenNumbers = numbers.stream().filter((number) -> number % 2==0).collect(Collectors.toList());
+		System.out.println(evenNumbers);
+
+		numbers.stream().filter(number -> number % 2==0).forEach(number -> System.out.println(number));
+		numbers.stream().filter(number -> number % 2 == 0).forEach(System.out::println);
 
 	}
-
 }
